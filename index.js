baf4ac812b7df0c1548a5ecc4d0ae2a1ab4f7958
Vesdet <vesdet@gmail.com>
2017-05-03 19:47:13 +0300

    Test CSRF

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const CORS_URL = 'http://127.0.0.1:1327/cors';
+const CSRF_URL = 'http://127.0.0.1:1327/csrf/customRequest';
+const CSRF_STEAL_COOKIE_URL = 'http://127.0.0.1:1327/csrf/getCookie';
 
 function sendCORS() {
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", 'application/xml');
     const body = '<?xml version="1.0"?><person><name>Nikita</name></person>';
+    const method = document.getElementById('cors-method').value;
 
     fetch(CORS_URL, {
-        method: 'post',
+        method: method,
         body: body,
         headers: myHeaders
     })
@@ -14,6 +17,39 @@ function sendCORS() {
         .then((data) => {
             console.log("serverData:", data);
             document.getElementById('corsText').innerHTML = JSON.stringify(data);
+        })
+        .catch((err) => document.getElementById('corsText').innerHTML = JSON.stringify(err));
+}
+
+function stealCookie() {
+    fetch(CSRF_STEAL_COOKIE_URL, {
+        credentials: 'include',
+        method: 'get'
+    })
+        .then((res) => res.text())
+        .then((data) => {
+            console.log("stealCookie:", data);
+            document.getElementsByClassName('stealing-cookies')[0].value = document.cookie;
+        })
+        .catch((err) => console.error(err));
+}
+
+function sendCSRF(form) {
+    let myHeaders = new Headers();
+    myHeaders.append("X-CSRF-TOKEN", form.csrfToken.value);
+
+    fetch(CSRF_URL, {
+        method: 'post',
+        body: JSON.stringify({
+            name: form.name.value,
+            year: form.year.value
+        }),
+        headers: myHeaders
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("serverData:", data);
+            document.getElementById('csrfText').innerHTML = JSON.stringify(data);
         })
         .catch((err) => console.error(err));
 }
--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ let url = require('url');
 
 let server = new http.Server(accept);
 
+const cookie = 'CSRF-TOKEN=TokenMD5withSalt';
 const data = {
     bachelor: true,
     name: 'Nikita',
     year: 2017
 };
-const allowDomains = ['http://google.com', 'http://localhost:1337'];
+const allowDomains = ['http://google.com', 'http://127.0.0.1:1337']; // Resolved domains for requests
 
 function accept(req, res) {
 
@@ -24,25 +25,68 @@ function accept(req, res) {
                 'Cache-Control': 'no-cache',
 
                 // Without 'Access-Control-Allow-Origin' - CORS not works
-                // 'Access-Control-Allow-Origin': '*'   // BAD
+                // 'Access-Control-Allow-Origin': '*',   // BAD
                 'Access-Control-Allow-Origin': allowDomains.includes(originDomain) ? originDomain : null,  // RIGHT
 
-                'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type',
-                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-                'Access-Control-Allow-Credentials': true
+                'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type', // Resolved headers
+                'Access-Control-Allow-Methods': 'GET, POST' // Permission only GET and POST methods
             });
             res.end(JSON.stringify(data));
+            break;
+
+        case '/csrf/getCookie':
+            console.info("request for cookie");
+            res.writeHead(200, {
+                'Access-Control-Allow-Origin': allowDomains.includes(originDomain) ? originDomain : null,
+                'Access-Control-Allow-Credentials': true,
+                'Set-Cookie': cookie + '; path=/;'
+            });
+            res.end('You are get Cookie');
+            break;
+
+        case '/csrf/customRequest':
+            let headers = {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'X-CSRF-TOKEN'
+            };
+            if (req.method === 'OPTIONS') {
+                res.writeHead(200, headers);
+                res.end();
+                break;
+            }
+            if (!req.headers["x-csrf-token"] || req.headers["x-csrf-token"] !== 'TokenMD5withSalt') {
+                console.info("request with incorrect token - " + req.headers["x-csrf-token"]);
+                res.writeHead(403, headers);
+                res.end(JSON.stringify({error: "Incorrect token"}));
+                break;
+            }
+            let reqBody = "";
+            req.on('data', chunk => reqBody += chunk); // Do smth with request data
+            req.on('end', () => {
+                res.writeHead(200, headers);
+                res.end(JSON.stringify(data));
+            });
             break;
 
         default:
             res.writeHead(200, {
                 'Content-Type': 'text/plain',
                 'Cache-Control': 'no-cache',
-                'Access-Control-Allow-Origin': 'https://www.google.ru/ http://localhost:63342'
+                'Access-Control-Allow-Origin': 'https://www.google.ru/'
             });
             res.end('Server responce');
     }
 }
+
+const fromGetDataToObj = (data) => {
+    let fields = data.split('&');
+    let field = null;
+    return fields.reduce((res, item) => {
+        field = item.split('=');
+        res[field[0]] = field[1];
+        return res;
+    }, {})
+};
 
 server.listen(1327, '127.0.0.1');
 console.log('Server running on port 1327');
